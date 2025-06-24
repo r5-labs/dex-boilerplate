e6f23d7afcab2be77c6c2e5429bc35a4805ed3a0
ZNX <henrique.baronceli@gmail.com>
2025-06-25 10:47:17 +1200

    base framework

---
+++ src/constants/lists.ts
@@ -0,0 +1,6 @@
+// Default Tokens List for R5 Network
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/r5-labs/common-assets/main/token-lists/public/_boilerplate.json'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+]
